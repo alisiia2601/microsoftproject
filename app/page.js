a7cd88ff8c89b8341a54146bb3e9467b4c7ac38c
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-03-21 22:28:51 +0100

    add styling instructions page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,19 @@
 // import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Instructions from './compontents/instructions/page'
 import styles from './page.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  style: 'normal',
+  weight: ['200', '400', '600', '700', '800'],
+})
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Hej</h1>
+    <main className={`${styles.main} ${inter.className}`}>
+      <Instructions />
+      {/* <h1>Hej</h1> */}
     </main>
   )
 }
