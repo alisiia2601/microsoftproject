f5ce2bc48c4e74a47b10ebf36ad0a385c30dd3b5
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-03-23 18:50:53 +0100

    final styling landing

--- app/components/landing/Landing.jsx
+++ app/components/landing/Landing.jsx
@@ -1,4 +1,4 @@
-import Image from "next/legacy/image"
+import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../landing/Landing.module.css'
 import backgroundImage from '/public/images/bakgrundsbild.png'
@@ -8,50 +8,44 @@ import { HiArrowLongRight } from 'react-icons/hi2'
 
 export default function Landing() {
   return (
-    <>
-      <div className={styles.mainContainer}>
-        <div className={styles.logo}>
-        <Image src={logo} 
-          alt="logo" 
-          layout='intrinsic'
-          priority='true'
-        />  
-        </div>
-        <div className={styles.overlay}>
-        </div>
-        <div className={styles.landingimage}>
-          <Image src={backgroundImage}
+    <div className={styles.mainContainer}>
+      <div className={styles.logo}>
+        <Image src={logo} alt="logo" width={107} />
+      </div>
+      <div className={styles.overlay}></div>
+      <div className={styles.landingimage}>
+        <Image src={backgroundImage}
             alt="background image"
             layout='fill'
             objectFit='cover'
             priority='true'
-          />
-        </div>
-        <div className={styles.circlesContainer}>
-            <Image 
-            src={circles}
-            className={styles.circles}
-            alt="circle images"
-            priority="true"
-            />
-        </div>
-        <div className={styles.text}>
+        /> 
+      </div>
+      <div className={styles.circlesContainer}>
+        <Image 
+          src={circles}
+          className={styles.circles}
+          alt="circle images"
+          priority="true"
+        />  
+      </div>
+      <div className={styles.text}>
           <p>Här finns jobben</p>
           <span>Välkommen hem!</span>
           <p>Arbetsmarknaden i Skellefteå blomstrar och behöver dig och din kompetens.</p>
-        </div>
+      </div>
       <Link href={'/Instructions'}>
-        <button className={styles.hitta}>
+        <button className={styles.button}>
           <p>Hitta ditt nästa drömjobb</p>
           <HiArrowLongRight 
             size={27}
             style={{
-              color: '#000'
+              color: '#000',
+              marginLeft: '63px'
             }}
           />
         </button>
       </Link>
-      </div>
-    </>
+    </div>
   )
 }
