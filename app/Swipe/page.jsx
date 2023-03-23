73f9a293a5b62e3e7381db6a1a90c3fb43a82dcd
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-03-23 21:46:31 +0100

    redo structe

--- app/Swipe/page.jsx
+++ app/Swipe/page.jsx
@@ -4,12 +4,10 @@ import Swiper from '../components/swiper/Swiper'
 import { Jobs } from '../../data/jobsArray'
 import styles from './swipe.module.css'
 import Footer from '/app/components/footer/Footer.jsx'
-import Header from '/app/components/header2/Header2.jsx'
 
 export default function Swipe () {
   return (
       <div className={styles.swipe_wrapper}>
-          <Header />
           <Swiper data={Jobs}  />
           <Footer />
     </div>
