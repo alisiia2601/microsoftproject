1430bebb700e61c367e8ea358254f4aa7283d305
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-03-23 22:57:01 +0100

    big fix vercel

--- app/Swipe/page.jsx
+++ app/Swipe/page.jsx
@@ -4,7 +4,7 @@ import Swiper from '../components/swiper/Swiper'
 import { Jobs } from '../../data/jobsArray'
 import styles from './swipe.module.css'
 import Footer from '/app/components/footer/Footer.jsx'
-import Header from '/app/components/Header/Header.jsx'
+import Header from '/app/components/header/Header.jsx'
 
 export default function Swipe () {
   return (
