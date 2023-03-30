89832b8dae3f36d8c8d415f709d1c7f2d702300f
robonexx <robertwagar@gmail.com>
2023-03-30 12:12:57 +0200

    topbar in components, importen in swipe page

--- app/Swipe/page.jsx
+++ app/Swipe/page.jsx
@@ -5,14 +5,16 @@ import Swiper from '../components/swiper/Swiper'
 import styles from './swipe.module.css'
 import Footer from '/app/components/footer/Footer.jsx'
 import Header from '/app/components/header/Header.jsx'
+import TopBar from '../components/topbar/TopBar'
 
 export default function Swipe() {  
  
   return (
       <div className={styles.swipe_wrapper}>
-          <Header />
-          <Swiper />
-          <Footer />
+        <Header />
+        <TopBar />
+        <Swiper />
+        <Footer />
     </div>
   )
 }
