48946aba51facc550816c21440eb944db90cc594
robonexx <robertwagar@gmail.com>
2023-03-30 10:04:17 +0200

    saving, will change, not ready

--- app/components/swiper/SwipeButtons.jsx
+++ app/components/swiper/SwipeButtons.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import { RiBookmarkLine, RiCloseLine } from 'react-icons/ri';
 import { MdOutlineRefresh } from 'react-icons/md';
-
+import { Jobs } from '@/data/jobsArray';
 import styles from './SwipeButtons.module.css'
 
-const SwipeButtons = () => {
+const SwipeButtons = ({ setJobData, onClickHandler }) => {
+  
+ /*  const { employer, role, desc, quali, img, id, link } = jobData */
 
     const Refresh = () => {
-        console.log('to refresh jobs / cards');
+      console.log('to refresh jobs / cards');
+      setJobData(Jobs)
       };
     
       const doSomething = () => {
         console.log('function to remove job from array goes here');
       };
-    
+    /* 
       function handleSave() {
         console.log('Function for saving job to db or localstorage, cookie etc');
-    }
+        saveJob(employer, role, desc, quali, img, id, link)
+    } */
     
   return (
     <div className={styles.iconsWrapper}>
@@ -24,7 +28,7 @@ const SwipeButtons = () => {
       <MdOutlineRefresh onClick={() => Refresh()} />
     </div>
     <div className={styles.iconWrapper}>
-      <RiBookmarkLine onClick={() => handleSave()} />
+      <RiBookmarkLine onClick={() => onClickHandler()} />
     </div>
     <div className={styles.iconWrapper}>
       <RiCloseLine onClick={() => doSomething()} />
--- app/components/swiper/Swiper.jsx
+++ app/components/swiper/Swiper.jsx
@@ -48,7 +48,12 @@ const Swiper = () => {
 
       const onCardLeftScreen = (myIdentifier) => {
         console.log('id: ' + myIdentifier + ' left the screen')
-    }
+  }
+  
+  const onClickHandler = () => {
+    saveJob(jobData.employer, jobData.role, jobData.desc, jobData.quali, jobData.img, jobData.id, jobData.link)
+    console.log('ät bajs')
+  }
     
     const swiped = (dir,  employer, role, desc, quali, img, id, link) => {
        /*  console.log('id is : ' + id, ' direction is : ' + dir) */
@@ -119,7 +124,7 @@ const Swiper = () => {
                 </div>                        
                 ))
           } 
-          <SwipeButtons />
+          <SwipeButtons setJobData={setJobData} onClickHandler={onClickHandler} />
         </div>
         </>
     )
